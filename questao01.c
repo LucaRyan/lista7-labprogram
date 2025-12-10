8937402555325a65fa015bcf6e42a81d134e59e5
LucaRyan <lucaryan2006@gmail.com>
2025-12-10 15:06:40 -0300

    Refactor program to calculate student grades

--- questao01.c
+++ questao01.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int main(){
-    
-    int numero;
+struct aluno {
+    float nota1;
+    float nota2;
+    float media;
+};
 
-    printf("Digite um numero de valor real: ");
-    scanf("%d", &numero);
+int main() {
 
-    printf("O valor em hexadecial é: %X\n", numero);
-    printf("O valor em octal é : %o\n", numero);
+    struct aluno meuAluno;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &meuAluno.nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &meuAluno.nota2);
+
+    meuAluno.media = (meuAluno.nota1 + meuAluno.nota2) / 2.0;
+
+    printf("\n--- Dados do Aluno ---\n");
+    printf("Nota 1: %.2f\n", meuAluno.nota1);
+    printf("Nota 2: %.2f\n", meuAluno.nota2);
+    printf("Média: %.2f\n", meuAluno.media);
 
     return 0;
-
 }
