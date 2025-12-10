319454a880a79288bde2ea709606eaf3aca20ec5
LucaRyan <lucaryan2006@gmail.com>
2025-12-10 15:10:49 -0300

    Refactor to calculate student averages

--- questao04.c
+++ questao04.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 
-int main(){
-    int valor_conta, valor_conta_att;
+#define NUM_ALUNOS 3
 
-    printf("Digite o valor da sua conta: ");
-    scanf ("%d", &valor_conta);
+struct aluno {
+    float nota1;
+    float nota2;
+    float media;
+};
 
-    valor_conta_att = valor_conta * 1.10;
+void calcularMediaAluno(struct aluno *a) {
+    a->media = (a->nota1 + a->nota2) / 2.0;
+}
 
-    printf("O valor da conta atualizado é: %1.d\n", valor_conta_att);
+int main() {
+    struct aluno turma[NUM_ALUNOS];
+
+    printf("--- Cadastro e Cálculo de Médias ---\n");
+    for (int i = 0; i < NUM_ALUNOS; i++) {
+        printf("\nAluno %d:\n", i + 1);
+
+        printf("Digite a primeira nota: ");
+        scanf("%f", &turma[i].nota1);
+
+        printf("Digite a segunda nota: ");
+        scanf("%f", &turma[i].nota2);
+
+        calcularMediaAluno(&turma[i]);
+    }
+
+    printf("\n--- Resumo da Turma ---\n");
+    for (int i = 0; i < NUM_ALUNOS; i++) {
+        printf("Aluno %d - Notas: %.2f e %.2f | Média: %.2f\n",
+               i + 1, turma[i].nota1, turma[i].nota2, turma[i].media);
+    }
 
     return 0;
 }
