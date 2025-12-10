1c9d10f853c7640d918417e0bfe1daf6560310c7
LucaRyan <lucaryan2006@gmail.com>
2025-12-10 15:07:23 -0300

    Implement inventory management structure and sample data
    Added a struct to manage inventory items and initialized an instance with sample data.

--- questao02.c
+++ questao02.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+e
+struct estoque {
+    char nomePeca[50];  // string com o nome Peca
+    int numeroPeca;     // número inteiro para identificar o número da peça
+    float preco;        // preço em ponto flutuante
+    int numeroPedido;   // elemento inteiro para identificar o número do pedido
+};
 
-int main(){
-    
-    float numero;
+int main() {
+    struct estoque item1;
 
-    printf("Digite um numero de valor real: ");
-    scanf("%f", &numero);
+    strcpy(item1.nomePeca, "Parafuso M8");
+    item1.numeroPeca = 1001;
+    item1.preco = 0.55;
+    item1.numeroPedido = 4523;
 
-    printf("Valor com a casa decimal: %1.f\n", numero);
+    printf("\n--- Dados do Estoque ---\n");
+    printf("Nome da Peça: %s\n", item1.nomePeca);
+    printf("Número da Peça: %d\n", item1.numeroPeca);
+    printf("Preço: R$%.2f\n", item1.preco);
+    printf("Número do Pedido: %d\n", item1.numeroPedido);
 
     return 0;
-
 }
